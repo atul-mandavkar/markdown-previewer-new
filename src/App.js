44583f76c3e0fb2e175f59cd54ad9ce47140f6ac
atul-mandavkar <76526334+atul-mandavkar@users.noreply.github.com>
2021-12-04 21:12:21 +0530

    Display style and features complited
    Completed minimize and maximize screen setting.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,46 @@ import EdittorContainer from "./components/EdittorContainer";
 import PreviewContainer from "./components/PreviewContainer";
 import BoardTaskbar from "./components/BoardTaskbar";
 import TaskbarChildren from "./components/TakbarChildren";
+import TaskbarIcons from "./components/TaskbarIcons";
 
 const App = () => {
+  const maximizeScreen1 = () => {
+    console.log("maximize1");
+    document.getElementsByClassName("maximize1")[0].style.display="none";
+    document.getElementsByClassName("minimize1")[0].style.display="inline";
+    let ele1 = document.getElementsByClassName("maximize1")[0].parentNode.parentNode.parentNode;
+    ele1.style.width="90vw";
+    ele1.style.height="100vh";
+  }
+  const minimizeScreen1 = () => {
+    console.log("minimize1");
+    document.getElementsByClassName("minimize1")[0].style.display="none";
+    document.getElementsByClassName("maximize1")[0].style.display="inline";
+    let ele1 = document.getElementsByClassName("minimize1")[0].parentNode.parentNode.parentNode;
+    ele1.style.width="60%";
+    ele1.style.height="40%";
+  }
+  const maximizeScreen2 = () => {
+    console.log("maximize2");
+    document.getElementsByClassName("maximize1")[1].style.display="none";
+    document.getElementsByClassName("minimize1")[1].style.display="inline";
+    let ele1 = document.getElementsByClassName("maximize1")[1].parentNode.parentNode.parentNode;
+    ele1.style.width="90vw";
+    ele1.style.height="100vh";
+    ele1.style.left="5vw";
+    document.getElementsByClassName("editContainer")[0].style.display="none";
+  }
+  const minimizeScreen2 = () => {
+    console.log("minimize2");
+    document.getElementsByClassName("minimize1")[1].style.display="none";
+    document.getElementsByClassName("maximize1")[1].style.display="inline";
+    let ele1 = document.getElementsByClassName("minimize1")[1].parentNode.parentNode.parentNode;
+    ele1.style.width="80%";
+    ele1.style.minHeight="50%";
+    ele1.style.left="10%";
+    document.getElementsByClassName("editContainer")[0].style.display="block";
+  }
+
   return (
     <Wrapper>
       <EdittorContainer>
@@ -13,7 +51,8 @@ const App = () => {
             <b>Edittor</b>
           </TaskbarChildren>
           <TaskbarChildren id="iconsContainerEdittor">
-            icons
+            <TaskbarIcons className="fas fa-compress minimize1" id="min1" onClick={minimizeScreen1}></TaskbarIcons>
+            <TaskbarIcons className="fas fa-expand maximize1" id="max1" onClick={maximizeScreen1}></TaskbarIcons>
           </TaskbarChildren>
         </BoardTaskbar>
       </EdittorContainer>
@@ -23,7 +62,8 @@ const App = () => {
             <b>Preview</b>
           </TaskbarChildren>
           <TaskbarChildren id="iconsContainerPreview">
-            icons
+            <TaskbarIcons className="fas fa-compress minimize1" id="min2" onClick={minimizeScreen2}></TaskbarIcons>
+            <TaskbarIcons className="fas fa-expand maximize1" id="max2" onClick={maximizeScreen2}></TaskbarIcons>
           </TaskbarChildren>
         </BoardTaskbar>
       </PreviewContainer>
---
+++ src/components/TaskbarIcons.js
@@ -0,0 +1,13 @@
+import "./TaskbarIcons.css";
+
+
+
+
+const TaskbarIcons = ({className, id, onClick}) => {
+  return <i className={className} id={id} onClick={onClick}></i>
+};
+
+
+
+
+export default TaskbarIcons;
